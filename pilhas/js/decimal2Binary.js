1fc27a9d5d01a162730a2534672c0c777e2c735b
José Lopes <946393361bill@gmail.com>
2022-12-13 23:53:33 +0000

    convertendo de decimal para binario usando pilha

--- pilhas/js/decimal2Binary.js
+++ pilhas/js/decimal2Binary.js
@@ -1,1 +1,18 @@
-//Utilizando o conceito de pilha para converter número decimal para binário
+// Utilizando o conceito de pilha para converter número decimal para binário
+
+function dec2Bin(decNumber){
+    var restStack = []
+    rest = ''
+    binaryString = '';
+
+    while(decNumber > 0){
+        rest = Math.floor(decNumber % 2)
+        restStack.push(rest)
+        decNumber = Math.floor(decNumber / 2)
+    }
+    while(restStack.length > 0) {
+        binaryString += restStack.pop().toString()
+    }
+    return binaryString
+}
+console.log(dec2Bin(10))
