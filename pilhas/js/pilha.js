8967277742f960e6347bec9cd1ea41d6fb19ef33
José Lopes <946393361bill@gmail.com>
2022-12-13 22:44:19 +0000

    first commit

---
+++ pilhas/js/pilha.js
@@ -0,0 +1,32 @@
+function Stack() {
+    var itens = []
+
+    this.push = function(element) {
+        // adiciona um novo item à pilha
+
+    }
+
+    this.pop = function() {
+        //remover o item do topo da pilha
+    }
+
+    this.peek = function() {
+        // devolve o elemento que está no topo da pilha
+    }
+
+    this.isEmpty = function() {
+        //informar se a pilha está vazia ou não
+    }
+
+    this.clear = function(){
+        //limpa a pilha
+    }
+
+    this.size = function() {
+        //informa o tamanho da pilha
+    }
+
+    this.print = function() {
+        // imprime a pilha no console
+    }
+}
