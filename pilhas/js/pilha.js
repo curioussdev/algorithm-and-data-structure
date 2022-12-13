1f1c9b2b904fc04b65ac80693847c18cdea26524
José Lopes <946393361bill@gmail.com>
2022-12-13 23:10:03 +0000

    implementação da class stack ou pilha

--- pilhas/js/pilha.js
+++ pilhas/js/pilha.js
@@ -3,30 +3,37 @@ function Stack() {
 
     this.push = function(element) {
         // adiciona um novo item à pilha
+        itens.push(element)
 
     }
 
     this.pop = function() {
         //remover o item do topo da pilha
+        itens.pop()
     }
 
     this.peek = function() {
         // devolve o elemento que está no topo da pilha
+        return itens[itens.length - 1]
     }
 
     this.isEmpty = function() {
         //informar se a pilha está vazia ou não
+        return itens.length === 0
     }
 
     this.clear = function(){
         //limpa a pilha
+         itens = []
     }
 
     this.size = function() {
         //informa o tamanho da pilha
+        return itens.length
     }
 
     this.print = function() {
         // imprime a pilha no console
+        console.log(itens.toString())
     }
 }
