7cda9d6c330549e12acfe09becd4780d1c41f501
Jose Lopes <946393361bill@gmail.com>
2023-01-02 23:40:15 +0000

    starting queue

---
+++ 2 - Queue/js/fila.js
@@ -0,0 +1,27 @@
+function queue(){
+    var items = [];
+
+    this.enqueue = function(){
+        
+    }
+
+    this.dequeue = function(){
+        // remove um item
+    }
+
+    this.front = function() {
+        // retorna o primeiro elemento da fila
+    }
+
+    this.isEmpty = function(){
+        // verifica se a fila está vazia ou não
+    }
+
+    this.size = function(){
+        // retorna o tamanho da fila
+    }
+
+    this.print = function(){
+        // imprime a fila no console
+    }
+}
