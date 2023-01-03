6cb0f1ff32c739aad0b365a3b8e1dea9e7567ef2
Jose Lopes <946393361bill@gmail.com>
2023-01-03 00:30:39 +0000

    introducing the concepts

--- 2 - Queue/js/fila.js
+++ 2 - Queue/js/fila.js
@@ -7,22 +7,27 @@ function queue(){
     }
 
     this.dequeue = function(){
-        // remove um item
+        // remove um item pela frente
+        return items.shift()
     }
 
     this.front = function() {
         // retorna o primeiro elemento da fila
+        return items[0]
     }
 
     this.isEmpty = function(){
         // verifica se a fila está vazia ou não
+        return items.length === 0
     }
 
     this.size = function(){
         // retorna o tamanho da fila
+        return items.length
     }
 
     this.print = function(){
         // imprime a fila no console
+        console.log(items.toString())
     }
 }
